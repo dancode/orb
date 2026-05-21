c5f4d041ac10173f90fbd24d9ce66d0e3f7a7755
dancode <dancode@gmail.com>
2026-05-20 18:30:50 -0700

    build_tool_gen: generate .vcxproj with filters
    Refactor build_tool_gen to scan source files and produce .vcxproj, .vcxproj.filters and .sln with proper folder filters. Replaces the old dynamic path_list_t approach with fixed-size file_info and filter arrays, computes filters per-path (including recursive parent filters), and emits ItemGroups for ClInclude/ClCompile and Filter entries. Also adds helper writers, simplifies file emission formatting, and wraps formatting control. Additionally, orb_make log updated to use sb_base_custom.exe / sb_base_custom.pdb as the build output.

--- source/tools/build_tool/build_tool_gen.c
+++ source/tools/build_tool/build_tool_gen.c
@@ -3,27 +3,78 @@
     build_tool_gen.c -- Generation of Visual Studio project files.
 
 ==============================================================================================*/
+// clang-format off
+
+#define MAX_FILES 512
+#define MAX_FILTERS 256
 
 typedef struct
 {
-    char** paths;
-    size_t count;
-    size_t cap;
-} path_list_t;
+    char path[ 256 ];
+    char filter[ 256 ];
+    bool is_header;
+} file_info_t;
+
+static file_info_t g_files[ MAX_FILES ];
+static int         g_file_count = 0;
+
+static char g_filters[ MAX_FILTERS ][ 256 ];
+static int  g_filter_count = 0;
 
 static void
-path_list_append( path_list_t* list, const char* path )
+add_filter( const char* filter )
 {
-    if ( list->count >= list->cap )
+    if ( filter[ 0 ] == '\0' ) return;
+    for ( int i = 0; i < g_filter_count; ++i )
     {
-        list->cap   = list->cap == 0 ? 256 : list->cap * 2;
-        list->paths = realloc( list->paths, list->cap * sizeof( char* ) );
+        if ( _stricmp( g_filters[ i ], filter ) == 0 ) return;
     }
-    list->paths[ list->count++ ] = _strdup( path );
+    if ( g_filter_count < MAX_FILTERS )
+    {
+        strcpy( g_filters[ g_filter_count++ ], filter );
+    }
 }
 
 static void
-scan_directory( const char* dir, path_list_t* cl_files, path_list_t* h_files )
+add_filters_recursive( const char* filter )
+{
+    char  tmp[ 256 ];
+    char* p = tmp;
+    strcpy( tmp, filter );
+    while ( *p )
+    {
+        if ( *p == '\\' )
+        {
+            *p = '\0';
+            add_filter( tmp );
+            *p = '\\';
+        }
+        p++;
+    }
+    add_filter( tmp );
+}
+
+static void
+get_filter_for_path( const char* path, char* out_filter )
+{
+    out_filter[ 0 ] = '\0';
+    if ( strncmp( path, "source/", 7 ) == 0 )
+    {
+        const char* sub        = path + 7;
+        const char* last_slash = strrchr( sub, '/' );
+        if ( last_slash )
+        {
+            size_t len = last_slash - sub;
+            strncpy( out_filter, sub, len );
+            out_filter[ len ] = '\0';
+            for ( char* p = out_filter; *p; ++p )
+                if ( *p == '/' ) *p = '\\';
+        }
+    }
+}
+
+static void
+scan_directory( const char* dir )
 {
     char search_path[ 512 ];
     sprintf( search_path, "%s/*", dir );
@@ -42,20 +93,23 @@ scan_directory( const char* dir, path_list_t* cl_files, path_list_t* h_files )
 
         if ( find_data.attrib & _A_SUBDIR )
         {
-            scan_directory( path, cl_files, h_files );
+            scan_directory( path );
         }
         else
         {
             const char* ext = strrchr( find_data.name, '.' );
             if ( ext )
             {
-                if ( _stricmp( ext, ".c" ) == 0 )
+                bool is_c = _stricmp( ext, ".c" ) == 0;
+                bool is_h = _stricmp( ext, ".h" ) == 0;
+
+                if ( ( is_c || is_h ) && g_file_count < MAX_FILES )
                 {
-                    path_list_append( cl_files, path );
-                }
-                else if ( _stricmp( ext, ".h" ) == 0 )
-                {
-                    path_list_append( h_files, path );
+                    file_info_t* f = &g_files[ g_file_count++ ];
+                    strcpy( f->path, path );
+                    f->is_header = is_h;
+                    get_filter_for_path( path, f->filter );
+                    if ( f->filter[ 0 ] != '\0' ) add_filters_recursive( f->filter );
                 }
             }
         }
@@ -63,6 +117,39 @@ scan_directory( const char* dir, path_list_t* cl_files, path_list_t* h_files )
     while ( _findnext( handle, &find_data ) == 0 );
 
     _findclose( handle );
+}
+
+static void
+write_vcxproj_file_group( FILE* f, const char* tag, bool headers )
+{
+    fprintf( f, "  <ItemGroup>\n" );
+    for ( int i = 0; i < g_file_count; ++i )
+    {
+        if ( g_files[ i ].is_header == headers )
+        {
+            fprintf( f, "    <%s Include=\"%s\" />\n", tag, g_files[ i ].path );
+        }
+    }
+    fprintf( f, "  </ItemGroup>\n" );
+}
+
+static void
+write_filters_file_group( FILE* f, const char* tag, bool headers )
+{
+    fprintf( f, "  <ItemGroup>\n" );
+    for ( int i = 0; i < g_file_count; ++i )
+    {
+        if ( g_files[ i ].is_header == headers )
+        {
+            fprintf( f, "    <%s Include=\"%s\">\n", tag, g_files[ i ].path );
+            if ( g_files[ i ].filter[ 0 ] != '\0' )
+            {
+                fprintf( f, "      <Filter>%s</Filter>\n", g_files[ i ].filter );
+            }
+            fprintf( f, "    </%s>\n", tag );
+        }
+    }
+    fprintf( f, "  </ItemGroup>\n" );
 }
 
 static void
@@ -74,12 +161,12 @@ build_gen_proj_build_tool( void )
     const char* build_tool_compile[] = {
         "source/tools/build_tool/build_tool.c",
     };
-    const char* build_tool_header[] = {
+    const char* build_tool_header[]  = {
         "source/tools/build_tool/build_tool.h",
         "source/tools/build_tool/build_tool_gen.c",
     };
     size_t build_tool_compile_count = sizeof( build_tool_compile ) / sizeof( build_tool_compile[ 0 ] );
-    size_t build_tool_header_count = sizeof( build_tool_header ) / sizeof( build_tool_header[ 0 ] );
+    size_t build_tool_header_count  = sizeof( build_tool_header ) / sizeof( build_tool_header[ 0 ] );
 
     FILE* f = fopen( vcxproj_path, "w" );
     if ( f )
@@ -106,19 +193,13 @@ build_gen_proj_build_tool( void )
         fprintf( f, "    <NMakePreprocessorDefinitions>OS_WINDOWS;COMPILER_MSVC;$(NMakePreprocessorDefinitions)</NMakePreprocessorDefinitions>\n" );
         fprintf( f, "    <NMakeIncludeSearchPath>$(ProjectDir)source;$(NMakeIncludeSearchPath)</NMakeIncludeSearchPath>\n" );
         fprintf( f, "  </PropertyGroup>\n" );
-        
+
         fprintf( f, "  <ItemGroup>\n" );
-        for ( size_t i = 0; i < build_tool_header_count; ++i )
-        {
-            fprintf( f, "    <ClInclude Include=\"%s\" />\n", build_tool_header[ i ] );
-        }
+        for ( size_t i = 0; i < build_tool_header_count; ++i ) fprintf( f, "    <ClInclude Include=\"%s\" />\n", build_tool_header[ i ] );
         fprintf( f, "  </ItemGroup>\n" );
 
         fprintf( f, "  <ItemGroup>\n" );
-        for ( size_t i = 0; i < build_tool_compile_count; ++i )
-        {
-            fprintf( f, "    <ClCompile Include=\"%s\" />\n", build_tool_compile[ i ] );
-        }
+        for ( size_t i = 0; i < build_tool_compile_count; ++i ) fprintf( f, "    <ClCompile Include=\"%s\" />\n", build_tool_compile[ i ] );
         fprintf( f, "  </ItemGroup>\n" );
 
         fprintf( f, "  <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.targets\" />\n" );
@@ -143,13 +224,13 @@ build_gen_proj_build_tool( void )
 static void
 build_gen_proj_engine( void )
 {
-    path_list_t cl_files = { 0 };
-    path_list_t h_files  = { 0 };
-    scan_directory( "source", &cl_files, &h_files );
+    g_file_count   = 0;
+    g_filter_count = 0;
+    scan_directory( "source" );
 
+    // --- 1. Project File (.vcxproj) ---
     char vcxproj_path[ 256 ];
     sprintf( vcxproj_path, "%s.vcxproj", g_proj_name );
-
     FILE* f = fopen( vcxproj_path, "w" );
     if ( f )
     {
@@ -175,20 +256,41 @@ build_gen_proj_engine( void )
         fprintf( f, "    <NMakePreprocessorDefinitions>OS_WINDOWS;COMPILER_MSVC;$(NMakePreprocessorDefinitions)</NMakePreprocessorDefinitions>\n" );
         fprintf( f, "    <NMakeIncludeSearchPath>$(ProjectDir)source;$(NMakeIncludeSearchPath)</NMakeIncludeSearchPath>\n" );
         fprintf( f, "  </PropertyGroup>\n" );
-        
-        fprintf( f, "  <ItemGroup>\n" );
-        for ( size_t i = 0; i < h_files.count; ++i ) fprintf( f, "    <ClInclude Include=\"%s\" />\n", h_files.paths[ i ] );
-        fprintf( f, "  </ItemGroup>\n" );
 
-        fprintf( f, "  <ItemGroup>\n" );
-        for ( size_t i = 0; i < cl_files.count; ++i ) fprintf( f, "    <ClCompile Include=\"%s\" />\n", cl_files.paths[ i ] );
-        fprintf( f, "  </ItemGroup>\n" );
+        write_vcxproj_file_group( f, "ClInclude", true );
+        write_vcxproj_file_group( f, "ClCompile", false );
 
         fprintf( f, "  <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.targets\" />\n" );
         fprintf( f, "</Project>\n" );
         fclose( f );
     }
 
+    // --- 2. Filters File (.vcxproj.filters) ---
+    char filters_path[ 256 ];
+    sprintf( filters_path, "%s.vcxproj.filters", g_proj_name );
+    f = fopen( filters_path, "w" );
+    if ( f )
+    {
+        fprintf( f, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" );
+        fprintf( f, "<Project ToolsVersion=\"4.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">\n" );
+
+        fprintf( f, "  <ItemGroup>\n" );
+        for ( int i = 0; i < g_filter_count; ++i )
+        {
+            fprintf( f, "    <Filter Include=\"%s\">\n", g_filters[ i ] );
+            fprintf( f, "      <UniqueIdentifier>{%08X-0000-0000-0000-000000000000}</UniqueIdentifier>\n", (unsigned int)i );
+            fprintf( f, "    </Filter>\n" );
+        }
+        fprintf( f, "  </ItemGroup>\n" );
+
+        write_filters_file_group( f, "ClInclude", true );
+        write_filters_file_group( f, "ClCompile", false );
+
+        fprintf( f, "</Project>\n" );
+        fclose( f );
+    }
+
+    // --- 3. Solution File (.sln) ---
     char sln_path[ 256 ];
     sprintf( sln_path, "%s.sln", g_proj_name );
     f = fopen( sln_path, "w" );
@@ -201,11 +303,6 @@ build_gen_proj_engine( void )
         fprintf( f, "Global\n\tGlobalSection(SolutionConfigurationPlatforms) = preSolution\n\t\tDebug|x64 = Debug|x64\n\t\tRelease|x64 = Release|x64\n\tEndGlobalSection\nEndGlobal\n" );
         fclose( f );
     }
-
-    for ( size_t i = 0; i < cl_files.count; ++i ) free( cl_files.paths[ i ] );
-    for ( size_t i = 0; i < h_files.count; ++i ) free( h_files.paths[ i ] );
-    free( cl_files.paths );
-    free( h_files.paths );
 }
 
 void
@@ -219,4 +316,5 @@ build_gen_projects( void )
     printf( "Projects generated successfully.\n" );
 }
 
+// clang-format on
 /*============================================================================================*/
