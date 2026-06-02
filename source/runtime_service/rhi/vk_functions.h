8407176a014b8290dbfd4c27e79ce6d0fa4ee9f6
dancode <dancode@gmail.com>
2026-06-02 07:28:30 -0700

    Centralize Vulkan function declarations and loaders
    Introduce a macro-driven vk_functions.h that enumerates Vulkan exported, global, instance and device-level functions, and silence a Windows warning (4191). Update vk_library.c to define static PFN_* function pointers via those macros and implement loader helpers: vk_lib_exported_entry_points(), vk_lib_global_entry_points(), vk_lib_instance_entry_points(), and vk_lib_device_entry_points(). Each loader resolves symbols using sys_library_get_symbol, vkGetInstanceProcAddr or vkGetDeviceProcAddr and logs a descriptive error on failure.

--- source/runtime_service/rhi/vk_functions.h
+++ source/runtime_service/rhi/vk_functions.h
@@ -4,11 +4,196 @@
 
 
 ==============================================================================================*/
+#ifdef OS_WINDOWS
+    #pragma warning( disable : 4191 )
+#endif
 
-/*==============================================================================================
-    State
-==============================================================================================*/
+#if !defined( VK_EXPORTED_FUNCTION )
+    #define VK_EXPORTED_FUNCTION( fun )
+#endif
+
+VK_EXPORTED_FUNCTION( vkGetInstanceProcAddr ) /* root function for getting vk pointers */
+VK_EXPORTED_FUNCTION( vkEnumerateInstanceVersion )
+
+#undef VK_EXPORTED_FUNCTION
+
+/*============================================================================================*/
+
+#if !defined( VK_GLOBAL_LEVEL_FUNCTION )
+    #define VK_GLOBAL_LEVEL_FUNCTION( fun )
+#endif
+
+VK_GLOBAL_LEVEL_FUNCTION( vkEnumerateInstanceExtensionProperties )
+VK_GLOBAL_LEVEL_FUNCTION( vkEnumerateInstanceLayerProperties )
+VK_GLOBAL_LEVEL_FUNCTION( vkCreateInstance )
+
+#undef VK_GLOBAL_LEVEL_FUNCTION
+
+/*============================================================================================*/
+
+#if !defined( VK_INSTANCE_LEVEL_FUNCTION )
+    #define VK_INSTANCE_LEVEL_FUNCTION( fun )
+#endif
+
+// Instance
+VK_INSTANCE_LEVEL_FUNCTION( vkDestroyInstance )
+
+// Pysical Device
+VK_INSTANCE_LEVEL_FUNCTION( vkEnumeratePhysicalDevices )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceProperties )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceFeatures )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceQueueFamilyProperties )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceMemoryProperties )
+VK_INSTANCE_LEVEL_FUNCTION( vkCreateDevice )
+
+VK_INSTANCE_LEVEL_FUNCTION( vkGetDeviceProcAddr )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceSurfaceSupportKHR )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceSurfaceCapabilitiesKHR )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceSurfaceFormatsKHR )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceSurfacePresentModesKHR )
+
+#if defined( VK_USE_PLATFORM_WIN32_KHR )
+VK_INSTANCE_LEVEL_FUNCTION( vkCreateWin32SurfaceKHR )
+#endif
+
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceWin32PresentationSupportKHR )
+VK_INSTANCE_LEVEL_FUNCTION( vkDestroySurfaceKHR )
+
+VK_INSTANCE_LEVEL_FUNCTION( vkEnumerateDeviceExtensionProperties )
+VK_INSTANCE_LEVEL_FUNCTION( vkEnumerateDeviceLayerProperties )
+
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceFormatProperties )
+VK_INSTANCE_LEVEL_FUNCTION( vkGetPhysicalDeviceImageFormatProperties )
+
+#undef VK_INSTANCE_LEVEL_FUNCTION
+
+/*==========================================================================================*/
+
+#if !defined( VK_DEVICE_LEVEL_FUNCTION )
+    #define VK_DEVICE_LEVEL_FUNCTION( fun )
+#endif
+
+// Device
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyDevice )
+VK_DEVICE_LEVEL_FUNCTION( vkGetDeviceMemoryCommitment )
+
+// KHR Swapchain
+VK_DEVICE_LEVEL_FUNCTION( vkCreateSwapchainKHR )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroySwapchainKHR )
+VK_DEVICE_LEVEL_FUNCTION( vkGetSwapchainImagesKHR )
+VK_DEVICE_LEVEL_FUNCTION( vkAcquireNextImageKHR )
+VK_DEVICE_LEVEL_FUNCTION( vkQueuePresentKHR )
+
+// Syncronziation
+VK_DEVICE_LEVEL_FUNCTION( vkCreateSemaphore )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroySemaphore )
+VK_DEVICE_LEVEL_FUNCTION( vkCreateFence )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyFence )
+VK_DEVICE_LEVEL_FUNCTION( vkWaitForFences )
+VK_DEVICE_LEVEL_FUNCTION( vkResetFences )
+VK_DEVICE_LEVEL_FUNCTION( vkGetFenceStatus )
+
+VK_DEVICE_LEVEL_FUNCTION( vkDeviceWaitIdle )
+
+// Shader
+VK_DEVICE_LEVEL_FUNCTION( vkCreateShaderModule )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyShaderModule )
+
+// Queue
+VK_DEVICE_LEVEL_FUNCTION( vkGetDeviceQueue )
+VK_DEVICE_LEVEL_FUNCTION( vkQueueSubmit )
+
+// Image
+VK_DEVICE_LEVEL_FUNCTION( vkCreateImageView )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyImageView )
+
+// RenderPass
+VK_DEVICE_LEVEL_FUNCTION( vkCreateRenderPass )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyRenderPass )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdBeginRenderPass )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdEndRenderPass )
+
+VK_DEVICE_LEVEL_FUNCTION( vkCreateFramebuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyFramebuffer )
+
+// Pipeline
+VK_DEVICE_LEVEL_FUNCTION( vkCreatePipelineLayout )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyPipelineLayout )
+VK_DEVICE_LEVEL_FUNCTION( vkCreateGraphicsPipelines )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyPipeline )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdBindPipeline )
 
 
+// VK_DEVICE_LEVEL_FUNCTION( vkCmdPipelineBarrier )
+
+// Command
+VK_DEVICE_LEVEL_FUNCTION( vkCreateCommandPool )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyCommandPool )
+VK_DEVICE_LEVEL_FUNCTION( vkResetCommandPool )
+
+VK_DEVICE_LEVEL_FUNCTION( vkAllocateCommandBuffers )
+VK_DEVICE_LEVEL_FUNCTION( vkFreeCommandBuffers )
+VK_DEVICE_LEVEL_FUNCTION( vkResetCommandBuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkBeginCommandBuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkEndCommandBuffer )
+
+
+VK_DEVICE_LEVEL_FUNCTION( vkQueueWaitIdle )
+
+// Draw
+VK_DEVICE_LEVEL_FUNCTION( vkCmdBlitImage )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdDraw )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdDrawIndexed )
+
+// Allocate
+VK_DEVICE_LEVEL_FUNCTION( vkAllocateMemory )
+VK_DEVICE_LEVEL_FUNCTION( vkFreeMemory )
+VK_DEVICE_LEVEL_FUNCTION( vkMapMemory )
+VK_DEVICE_LEVEL_FUNCTION( vkUnmapMemory )
+VK_DEVICE_LEVEL_FUNCTION( vkFlushMappedMemoryRanges )
+
+// Buffers
+VK_DEVICE_LEVEL_FUNCTION( vkCreateBuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyBuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdCopyBuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkGetBufferMemoryRequirements )
+VK_DEVICE_LEVEL_FUNCTION( vkBindBufferMemory )
+
+// VK_DEVICE_LEVEL_FUNCTION( vkCmdClearColorImage )
+
+// Commands
+VK_DEVICE_LEVEL_FUNCTION( vkCmdSetViewport )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdSetScissor )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdBindVertexBuffers )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdBindIndexBuffer )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdClearColorImage )
+
+// Image
+VK_DEVICE_LEVEL_FUNCTION( vkCreateImage )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyImage )
+VK_DEVICE_LEVEL_FUNCTION( vkGetImageMemoryRequirements )
+VK_DEVICE_LEVEL_FUNCTION( vkBindImageMemory )
+VK_DEVICE_LEVEL_FUNCTION( vkGetImageSubresourceLayout )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdCopyBufferToImage )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdPipelineBarrier )
+
+// Sampler
+VK_DEVICE_LEVEL_FUNCTION( vkCreateSampler )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroySampler )
+
+// Descriptor
+VK_DEVICE_LEVEL_FUNCTION( vkCreateDescriptorSetLayout )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyDescriptorSetLayout )
+VK_DEVICE_LEVEL_FUNCTION( vkCreateDescriptorPool )
+VK_DEVICE_LEVEL_FUNCTION( vkDestroyDescriptorPool )
+VK_DEVICE_LEVEL_FUNCTION( vkAllocateDescriptorSets )
+VK_DEVICE_LEVEL_FUNCTION( vkUpdateDescriptorSets )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdBindDescriptorSets )
+VK_DEVICE_LEVEL_FUNCTION( vkCmdPushConstants )
+
+// VK_KHR_push_descriptor
+VK_DEVICE_LEVEL_FUNCTION( vkCmdPushDescriptorSetKHR )
+
+#undef VK_DEVICE_LEVEL_FUNCTION
 
 /*============================================================================================*/
--- source/runtime_service/rhi/vk_library.c
+++ source/runtime_service/rhi/vk_library.c
@@ -4,11 +4,110 @@
 
 ==============================================================================================*/
 
-/*==============================================================================================
-    Includes
-==============================================================================================*/
+/* forward declare vulkan function pointrs */
+
+#define VK_EXPORTED_FUNCTION( fun )       static PFN_##fun fun;
+#define VK_GLOBAL_LEVEL_FUNCTION( fun )   static PFN_##fun fun;
+#define VK_INSTANCE_LEVEL_FUNCTION( fun ) static PFN_##fun fun;
+#define VK_DEVICE_LEVEL_FUNCTION( fun )   static PFN_##fun fun;
 
 #include "runtime_service/rhi/vk_functions.h"
+
+/*============================================================================================*/
+
+static bool
+vk_lib_exported_entry_points()
+{
+    i8* func = NULL;
+
+#define VK_EXPORTED_FUNCTION( fun )                            \
+    fun = ( PFN_##fun )sys_library_get_symbol( vk.dll, #fun ); \
+    if ( !fun )                                                \
+    {                                                          \
+        func = #fun;                                           \
+        goto exit;                                             \
+    }
+
+#include "runtime_service/rhi/vk_functions.h"
+
+    return true;
+
+exit:
+    LOG_ERROR( "Could not load exported function: %s", func );
+    return false;
+}
+
+/*============================================================================================*/
+
+static bool
+vk_lib_global_entry_points()
+{
+    i8* func = NULL;
+
+#define VK_GLOBAL_LEVEL_FUNCTION( fun )                     \
+    fun = ( PFN_##fun )vkGetInstanceProcAddr( NULL, #fun ); \
+    if ( !fun )                                             \
+    {                                                       \
+        func = #fun;                                        \
+        goto exit;                                          \
+    }
+
+#include "runtime_service/rhi/vk_functions.h"
+
+    return true;
+
+exit:
+    LOG_ERROR( "Could not load global level function: %s", func );
+    return false;
+}
+
+/*============================================================================================*/
+
+static bool
+vk_lib_instance_entry_points()
+{
+    i8* func = NULL;
+
+#define VK_INSTANCE_LEVEL_FUNCTION( fun )                          \
+    fun = ( PFN_##fun )vkGetInstanceProcAddr( vk.instance, #fun ); \
+    if ( !fun )                                                    \
+    {                                                              \
+        func = #fun;                                               \
+        goto exit;                                                 \
+    }
+
+#include "runtime_service/rhi/vk_functions.h"
+
+    return true;
+
+exit:
+    LOG_ERROR( "Could not load instance level function: %s", func );
+    return false;
+}
+
+/*============================================================================================*/
+
+static bool
+vk_lib_device_entry_points()
+{
+    i8* func = NULL;
+
+#define VK_DEVICE_LEVEL_FUNCTION( fun )                        \
+    fun = ( PFN_##fun )vkGetDeviceProcAddr( vk.device, #fun ); \
+    if ( !fun )                                                \
+    {                                                          \
+        func = #fun;                                           \
+        goto exit;                                             \
+    }
+
+#include "runtime_service/rhi/vk_functions.h"
+
+    return true;
+
+exit:
+    LOG_ERROR( "Could not load device level function: %s", func );
+    return false;
+}
 
 /*=============================================================================================
     Library loading
