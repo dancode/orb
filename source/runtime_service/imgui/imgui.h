e974af31b591f264002d3b6cf44c79b9606fd8cd
dancode <dancode@gmail.com>
2026-06-22 10:07:53 -0700

    Align comments for table flags in imgui.h
    Adjust spacing in the comment alignment for IMGUI_TABLE_BORDERS_H, IMGUI_TABLE_BORDERS_V, and IMGUI_TABLE_BORDERS_OUTER in source/runtime_service/imgui/imgui.h. This is a non-functional formatting change to improve readability.

--- source/runtime_service/imgui/imgui.h
+++ source/runtime_service/imgui/imgui.h
@@ -764,9 +764,9 @@ typedef u32 imgui_table_flags_t;
 typedef enum
 {
     IMGUI_TABLE_NONE            = 0,
-    IMGUI_TABLE_BORDERS_H       = 1 << 0,   /* horizontal row dividers (between rows)          */
-    IMGUI_TABLE_BORDERS_V       = 1 << 1,   /* vertical column dividers (between columns)      */
-    IMGUI_TABLE_BORDERS_OUTER   = 1 << 2,   /* outer frame border around the whole table       */
+    IMGUI_TABLE_BORDERS_H       = 1 << 0,   /* horizontal row dividers (between rows)         */
+    IMGUI_TABLE_BORDERS_V       = 1 << 1,   /* vertical column dividers (between columns)     */
+    IMGUI_TABLE_BORDERS_OUTER   = 1 << 2,   /* outer frame border around the whole table      */
     IMGUI_TABLE_BORDERS         = IMGUI_TABLE_BORDERS_H | IMGUI_TABLE_BORDERS_V | IMGUI_TABLE_BORDERS_OUTER,
     IMGUI_TABLE_SCROLL_Y        = 1 << 3,   /* table body scrolls vertically                  */
     IMGUI_TABLE_SCROLL_X        = 1 << 4,   /* table body scrolls horizontally                */
